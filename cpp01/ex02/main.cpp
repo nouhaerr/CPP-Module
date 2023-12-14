ed1517ef20a3bfdf098ee94d821a84dd21750aa5
Nouhaila Errakeb <nerrakeb@e2r1p4.1337.ma>
2023-12-14 20:06:31 +0100

    ok

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,13 +6,26 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 19:09:16 by nerrakeb          #+#    #+#             */
-/*   Updated: 2023/12/12 19:12:57 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2023/12/14 20:05:09 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int	main(void)
 {
 	std::string	var = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &var;
+	std::string	&stringREF = var;
+
+	std::cout << "The memory address of the string variable: " << &var << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of the string variable: " << var << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+
+	return (0);
 }
