731152c93d97d4188f7c0bab30645be51b1d8a72
Nouhaila Errakeb <nerrakeb@e2r2p4.1337.ma>
2023-12-14 18:20:46 +0100

    ok

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 13:11:37 by nerrakeb          #+#    #+#             */
-/*   Updated: 2023/12/12 19:10:06 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2023/12/14 18:18:55 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 int	main(void)
 {
 	int		nbr = 5;
-	Zombie	*zombie = zombieHorde(5, "Zombie horde");
+	Zombie	*zombie = zombieHorde(nbr, "zombieHorde");
 
-	for (int i = 0; i < nbr; i++)
-		zombie[i].announce();
+	if (nbr > 0)
+	{
+		for (int i = 0; i < nbr; i++)
+			zombie[i].announce();
+	}
 	delete[] zombie;
 	return (0);
 }
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 13:07:09 by nerrakeb          #+#    #+#             */
-/*   Updated: 2023/12/13 20:13:34 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2023/12/14 18:19:48 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 Zombie*	zombieHorde(int N, std::string name)
 {
+	if (N <= 0)
+	{
+		std::cerr << "error: nbr should be positive!" << std::endl;
+		exit(0);
+	}
 	Zombie *z = new Zombie[N];
 
-	//check if N < 0 and if name is empty or NULL
 	for (int i = 0; i < N; i++)
 		z[i].setName(name);
 	return (z);
