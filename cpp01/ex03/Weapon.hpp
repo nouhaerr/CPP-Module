8dbebba1b9e2d0d044f7228a36a653a5e1fd4464
Nouhaila Errakeb <nerrakeb@e2r1p4.1337.ma>
2023-12-14 20:35:33 +0100

    ex03

---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 20:10:12 by nerrakeb          #+#    #+#             */
+/*   Updated: 2023/12/14 20:17:10 by nerrakeb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+# include <string>
+
+class Weapon {
+
+	private :
+		std::string	_type;
+
+	public :
+		Weapon(void);
+		~Weapon();
+		const std::string& getType(void) const;
+};
+
+#endif
