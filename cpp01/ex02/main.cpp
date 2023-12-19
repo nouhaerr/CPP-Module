8854472dc45cfb309139e52fceee170f8cb39bc8
nouhaila errakeb <nerrakeb@gmail.com>
2023-12-19 23:41:39 +0100

    ok

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -16,8 +16,8 @@
 int	main(void)
 {
 	std::string	var = "HI THIS IS BRAIN";
-	std::string	*stringPTR = &var;
-	std::string	&stringREF = var;
+	std::string* stringPTR = &var;
+	std::string& stringREF = var;
 
 	std::cout << "The memory address of the string variable: " << &var << std::endl;
 	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
