621a9ad5f7bbc1d7a0226af964762884cc9883f4
Nouhaila Errakeb <nerrakeb@e2r12p1.1337.ma>
2023-12-21 03:55:55 +0100

    ok2

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 20:10:10 by nerrakeb          #+#    #+#             */
-/*   Updated: 2023/12/15 18:21:57 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2023/12/21 03:34:40 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ Weapon::~Weapon(){}
 
 const std::string&	Weapon::getType(void) const
 {
-    return this->_type;
+	return this->_type;
 }
 
 void	Weapon::setType(const std::string type)
 {
-    this->_type = type;
+	this->_type = type;
 }
 
