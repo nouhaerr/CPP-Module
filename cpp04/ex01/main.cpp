73f6d3735b0935a969a1c85975dc979798adb9b7
Nouhaila Errakeb <nerrakeb@e2r12p1.1337.ma>
2024-01-11 02:59:08 +0100

    fin04

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 07:41:09 by nerrakeb          #+#    #+#             */
-/*   Updated: 2024/01/11 00:54:03 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2024/01/11 02:56:27 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,48 +19,70 @@
 
 int	main(void)
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	// const Animal* j = new Dog();
+	// const Animal* i = new Cat();
 
-	std::cout << std::endl;
+	// std::cout << std::endl;
 
-	delete j;
-	delete i;
+	// delete j;
+	// delete i;
 
-	std::cout << std::string(50, '-') << std::endl;
+	// std::cout << std::string(50, '-') << std::endl;
 
 
-	const int size = 4;
-	Animal* animals[size];
+	// const int size = 4;
+	// Animal* animals[size];
 
-	for (int i = 0; i < size / 2; ++i){
-		animals[i] = new Dog();
-	}
-	for (int i = size / 2; i < size; ++i) {
-		animals[i] = new Cat();
-	}
-	std::cout << std::endl;
-	for (int j = 0; j < size; ++j)
-		animals[j]->makeSound();
-	std::cout << std::endl;
+	// for (int i = 0; i < size / 2; ++i){
+	// 	animals[i] = new Dog();
+	// }
+	// for (int i = size / 2; i < size; ++i) {
+	// 	animals[i] = new Cat();
+	// }
+	// std::cout << std::endl;
+	// for (int j = 0; j < size; ++j)
+	// 	animals[j]->makeSound();
+	// std::cout << std::endl;
 
-	for (int i = 0; i < size; ++i)
-	{
-		delete animals[i];
-	}
-	std::cout << std::string(50, '-') << std::endl;
+	// for (int i = 0; i < size; ++i)
+	// {
+	// 	delete animals[i];
+	// }
+	// std::cout << std::string(50, '-') << std::endl;
+
+	// std::string *catIdeas = new std::string[100];   // test the deep copy and shallow copy
+	// catIdeas[0] = "cat idea : (i want to eat)";
+	// std::string *luluIdeas = new std::string[100];
+	// luluIdeas[0] = "no idea";
+
+	// Brain catBrain;
+	// catBrain.setIdeas(catIdeas);
+	// Brain br;
+	// br.setIdeas(luluIdeas);
+
+	// Cat c;
+	// c.setBrain(&catBrain);
+	// Cat b(c);
+
+	// c.setBrain(&br);
 
 	Cat c;
-	Cat b(c);
+	Cat b;
+	b = c;
 	std::cout << std::endl;
-
 	c.setType("Lulu Caty");
+	std::cout << std::string(50, '-') << std::endl;
 	std::cout << c.getBrain() << std::endl;
 	std::cout << b.getBrain() << std::endl;
 	std::cout << c.getType() << std::endl;
 	std::cout << b.getType() << std::endl;
 
 	std::cout << std::string(50, '-') << std::endl;
+	
+	// std::cout << c.getBrain()->getIdeas()[0] << std::endl;
+	// std::cout << b.getBrain()->getIdeas()[0] << std::endl;
+
+	// std::cout << std::string(50, '-') << std::endl;
 
 	return 0;
 }
