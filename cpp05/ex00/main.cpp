e1e8b9416207edb33abb376b92c64ede40255b39
Nouhaila Errakeb <nerrakeb@e2r12p1.1337.ma>
2024-01-12 23:02:52 +0100

    p04

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 20:56:54 by nerrakeb          #+#    #+#             */
-/*   Updated: 2024/01/12 22:06:37 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2024/01/12 22:44:54 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	main(void)
 {
-
 	try {
-			Bureaucrat	b("Cosmetics", 180);
-			
+			Bureaucrat	b("Cosmetics", 1);
 			std::cout << b << std::endl;
+			Bureaucrat	b1("Cosmetics", 180);
+			std::cout << b1 << std::endl;
 	}
 	catch (std::exception & e)
 	{
 		std::cout << "Exception: " << e.what() << std::endl;
 	}
+
 	return 0;
 }
