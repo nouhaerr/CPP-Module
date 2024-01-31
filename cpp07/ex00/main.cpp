26aa62aad31249c3941101e592b6557e95cd88a0
Nouhaila Errakeb <nerrakeb@e2r13p2.1337.ma>
2024-02-01 00:42:29 +0100

    ok

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 00:24:10 by nerrakeb          #+#    #+#             */
+/*   Updated: 2024/02/01 00:25:11 by nerrakeb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main(void)
+{
+		int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
